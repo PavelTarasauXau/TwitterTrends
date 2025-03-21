79a654d3c8d2d943b2804da45fdeccd1c7ce02f4
Artur Tazhun <tazhuna25@mail.ru>
2025-03-21 10:05:15 +0300

    rewrited code in module "draw_map.py" using oop approach added class "State" with method "Plot" added class "StatePlotter" that manage the proccess of loading input data and drawing map

--- draw_map.py
+++ draw_map.py
@@ -1,30 +1,45 @@
 
-
-
 import json
 import matplotlib.pyplot as plt
 
-# Загружаем JSON-файл
-with open("states.json", "r") as file:
-    states_data = json.load(file)
+class State:
+    def __init__(self, name, regions):
+        self.name = name
+        self.regions = regions
 
-# Создаем фигуру
-fig, ax = plt.subplots(figsize=(10, 8))
+    def plot(self, ax):
+        """Рисует границы штата на переданной оси."""
+        for region in self.regions:
+            for polygon in region:
+                lons, lats = zip(*polygon)
+                ax.plot(lons, lats, label=self.name)
 
-# Обрабатываем каждый штат
-for state, regions in states_data.items():
-    for region in regions:
-        for polygon in region:
-            # Разбираем координаты
-            lons, lats = zip(*polygon)  # Разделяем широту и долготу
-            ax.plot(lons, lats, label=state)
+class StatePlotter:
+    def __init__(self, json_file):
+        self.json_file = json_file
+        self.states = self.load_states()
 
-# Добавляем подписи и оформление
-ax.set_title("Границы штатов")
-ax.set_xlabel("Долгота")
-ax.set_ylabel("Широта")
-ax.legend()
-ax.grid(True)
+    def load_states(self):
+        """Загружает JSON-файл и создает объекты State."""
+        with open(self.json_file, "r") as file:
+            states_data = json.load(file)
+        return [State(name, regions) for name, regions in states_data.items()]
 
-# Отображаем карту
-plt.show()
+    def plot_states(self):
+        """Рисует границы всех штатов на графике."""
+        fig, ax = plt.subplots(figsize=(10, 8))
+
+        for state in self.states:
+            state.plot(ax)
+
+        ax.set_title("Границы штатов")
+        ax.set_xlabel("Долгота")
+        ax.set_ylabel("Широта")
+        ax.legend()
+        ax.grid(True)
+        plt.show()
+
+# Использование классов
+if __name__ == "__main__":
+    plotter = StatePlotter("states.json")
+    plotter.plot_states()
