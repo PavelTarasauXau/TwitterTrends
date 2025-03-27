9c07a9d77687801827bcc6d610097849475625d0
Artur Tazhun <tazhuna25@mail.ru>
2025-03-28 01:38:55 +0300

    modified file draw_map.py write the reusable and working parser of json data and try to draw a map, but smth went wrong.. need to change function that transform coordinates for canvas

--- draw_map.py
+++ draw_map.py
@@ -1,49 +1,124 @@
-
-
+from tkinter import *
+from tkinter import ttk
 import json
-import matplotlib.pyplot as plt
+import math
 
-class State:
-    def __init__(self, name, regions):
-        self.name = name
-        self.regions = regions
+root = Tk()
+root.title("TWITTER-TRENDS  ")
+root.geometry("1250x600")
+root.update_idletasks() # чтобы размеры применились до того как захочу их получить в качестве свойств окна
 
-    def plot(self, ax):
-        """Рисует границы штата на переданной оси."""
-        for region in self.regions:
-            for polygon in region:
-                if isinstance(polygon[0], list):  # Проверяем, является ли первый элемент списком координат
-                    lons, lats = zip(*polygon)
-                    ax.plot(lons, lats, label=self.name)
+canvas = Canvas(bg="white", width=root.winfo_width(), height=root.winfo_height())
+canvas.pack(anchor=CENTER,expand=1)
+
+lbl = Label(text='MAP OF THE STATES')
+lbl.config(
+    background='black',
+    anchor=CENTER,
+    borderwidth=2,
+    font=('Arial',15),
+    cursor='heart',
+    padx=20,
+    pady=10,
+    foreground='white'
+)
+root.update_idletasks()
+
+canvas.create_window(root.winfo_width()/2, 50, anchor=CENTER, window=lbl, width=300, height=50)
+
+root_icon = PhotoImage(file='data/imgs/twitter.png')
+root.iconphoto(False,root_icon)
+
+
+
+canvas.create_polygon(
+    10,10,
+    100,40,
+    200,100,
+    40,90,
+    10,10,
+    fill='white',
+    outline='black'
+)
+
+
+
+
+with open("states.json", "r") as file:
+    data = json.load(file)
+
+
+
+
+# начальные значения из файла
+x_min = -117.033359
+x_max = -117.033359
+y_min = 49.000239
+y_max = 49.000239
+
+
+# функция для перевода из земных координат (широты и долготы) в плоские координаты
+# и учёта границ canvas
+def transform(lon, lat):
+    """Преобразует координаты (lon, lat) в пиксели для Canvas"""
+    x = (lon - x_min) / (x_max - x_min) * root.winfo_width()
+    y = (1 - (lat - y_min) / (y_max - y_min)) * root.winfo_height()  # Инверсия Y
+    return [x, y]
+
+# первый ращ делаем цикл чтобы распарсить данные и сформировать границы координат
+#  проходимся по ключам
+for state in data: # ключи(названия штатов)
+    for x in data[state]: # список полигонов
+        for y in x: # контуры
+            for polygon in y: # полигон
+                if isinstance(polygon,list):
+                        if polygon[0] < x_min: x_min = polygon[0]
+                        elif polygon[0] > x_max: x_max = polygon[0]
+                        if polygon[1] < y_min: y_min = polygon[1]
+                        elif polygon[1] > y_max: y_max = polygon[1]
+
+                   # print( transform(*polygon) )
                 else:
-                    print(f"Ошибка в данных штата {self.name}: {polygon}")
+                    if y[0] < x_min:
+                        x_min = y[0]
+                    elif y[0] > x_max:
+                        x_max = y[0]
+                    if y[1] < y_min:
+                        y_min = y[1]
+                    elif y[1] > y_max:
+                        y_max = y[1]
+                   # print( transform(*y) )
 
-class StatePlotter:
-    def __init__(self, json_file):
-        self.json_file = json_file
-        self.states = self.load_states()
 
-    def load_states(self):
-        """Загружает JSON-файл и создает объекты State."""
-        with open(self.json_file, "r") as file:
-            states_data = json.load(file)
-        return [State(name, regions) for name, regions in states_data.items()]
 
-    def plot_states(self):
-        """Рисует границы всех штатов на графике."""
-        fig, ax = plt.subplots(figsize=(10, 8))
+line_params = {
+    "count":1,
+    "dot_1":None,
+    "dot_2":None
+}
+count = 0
+def draw_line(dot):
+    line_params[f'dot_{line_params["count"]}'] = dot
+    line_params['count'] +=1
+    if line_params['count'] == 3:
+        canvas.create_line(*line_params["dot_1"],*line_params["dot_2"])
+        line_params["count"] = 1 # обнуляем счётчик
+        line_params["dot_1"] = line_params["dot_2"] # делаем вторую точку первой и по новой
 
-        for state in self.states:
-            state.plot(ax)
+print(x_min)
+print(x_max)
+print(y_min)
+print(y_max)
 
-        ax.set_title("Границы штатов")
-        ax.set_xlabel("Долгота")
-        ax.set_ylabel("Широта")
-        ax.legend()
-        ax.grid(True)
-        plt.show()
+# второй цикл нужен для того чтобы уже с готовыми границами получать правильные координаты и рисовать карту
+for state in data: # ключи(названия штатов)
+    print(f'{state} \n')
+    for x in data[state]: # список полигонов
+        for y in x: # контуры
+            for polygon in y: # полигон
+                if isinstance(polygon,list):
+                   draw_line( transform(*polygon) )
+                else:
+                    draw_line( transform(*y) )
 
-# Использование классов
-if __name__ == "__main__":
-    plotter = StatePlotter("states.json")
-    plotter.plot_states()
+root.mainloop()
