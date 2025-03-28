5e65b270089aebdc2c9b2427c64fdc385f4d8733
Artur Tazhun <tazhuna25@mail.ru>
2025-03-29 00:13:12 +0300

    changed transform "function" for adapting coordinates from json file to canvas space - added "padding" and "scale" parametrs

--- draw_map.py
+++ draw_map.py
@@ -1,12 +1,13 @@
 from tkinter import *
 from tkinter import ttk
 import json
+import threading
 
 
 
 root = Tk()
 root.title("TWITTER-TRENDS  ")
-root.geometry("1920x1080")
+root.geometry("1520x780")
 root.update_idletasks() # чтобы размеры применились до того как захочу их получить в качестве свойств окна
 
 canvas = Canvas(bg="white", width=root.winfo_width(), height=root.winfo_height())
@@ -43,75 +44,70 @@ with open("states.json", "r") as file:
 
 
 # начальные значения из файла
-x_min = -117.033359
-x_max = -117.033359
-y_min = 49.000239
-y_max = 49.000239
+min_lon = -117.033359
+max_lon = -117.033359
+min_lat = 49.000239
+max_lat = 49.000239
 
 
 # функция для перевода из земных координат (широты и долготы) в плоские координаты
 # и учёта границ canvas
-def transform(lon, lat):
-    """Преобразует координаты (lon, lat) в пиксели для Canvas"""
-    x = (lon - x_min) / (x_max - x_min) * root.winfo_width()
-    y = (1 - (lat - y_min) / (y_max - y_min)) * root.winfo_height()  # Инверсия Y
+def transform(lon, lat, padding=50):
+    # Вычисляем коэффициенты масштабирования
+    scale_x = (root.winfo_width() - 2 * padding) / (max_lon - min_lon)  # Масштаб по ширине
+    scale_y = (root.winfo_height() - 2 * padding) / (max_lat - min_lat)  # Масштаб по высоте
+
+    # Чтобы карта не была сжатой, можно выравнивать масштабы
+    scale = min(scale_x, scale_y) * 3  # 1.2 растягивает карту немного
+
+    # Переводим координаты в плоскую систему
+    x = (lon - min_lon) * scale + padding
+    y = (max_lat - lat) * scale + padding  # Инверсия, так как y идёт вниз
+
     return [x, y]
+
 
 # первый ращ делаем цикл чтобы распарсить данные и сформировать границы координат
 #  проходимся по ключам,
 for state in data: # ключи(названия штатов)
-    for x in data[state]: # список полигонов
-        for y in x: # контуры
-            for polygon in y: # полигон
-                if isinstance(polygon,list):
-                    if polygon[0] < x_min: x_min = polygon[0]
-                    elif polygon[0] > x_max: x_max = polygon[0]
-                    if polygon[1] < y_min: y_min = polygon[1]
-                    elif polygon[1] > y_max: y_max = polygon[1]
-                else:
-                    if y[0] < x_min:
-                        x_min = y[0]
-                    elif y[0] > x_max:
-                        x_max = y[0]
-                    if y[1] < y_min:
-                        y_min = y[1]
-                    elif y[1] > y_max:
-                        y_max = y[1]
+    for polygon in data[state]: # полигон
+        for point in polygon: # точка полигона
+            if point[0] < min_lon: min_lon = point[0]
+            elif point[0] > max_lon: max_lon = point[0]
+            if point[1] < min_lat: min_lat = point[1]
+            elif point[1] > max_lat: max_lat = point[1]
 
 
 
 any_polygon = {
     'dots':[]
 }
-count = 0
+
 def draw_polygon(dot):
-    any_polygon['dots'].append(dot)
-    if dot in any_polygon['dots']:
+    if dot[0] in any_polygon['dots'] and dot[1] in any_polygon['dots']:
+        any_polygon['dots'].extend(dot)
         # рисуем полигон
-        canvas.create_polygon(*any_polygon['dots'],fill='red',outline='black')
+        print('рисую полигон')
+        print(any_polygon['dots'])
+        canvas.create_polygon(*any_polygon['dots'],fill='white',outline='black',width=1)
         # очищаем промежуточный объект
         any_polygon['dots'].clear()
+    else:
+        any_polygon['dots'].extend(dot)
 
 
-print(x_min)
-print(x_max)
-print(y_min)
-print(y_max)
-print('\n')
+
 
 # второй цикл нужен для того чтобы уже с готовыми границами получать правильные координаты и рисовать карту
 for state in data: # ключи(названия штатов)
     print(f'draw state - {state}')
-    for x in data[state]: # список полигонов
-        for y in x: # контуры
-            for polygon in y: # полигон
-                if isinstance(polygon,list):
-                    got_coords = transform(*polygon)
-                    print(got_coords)
-                    draw_polygon( got_coords )
-                else:
-                    got_coords = transform(*y)
-                    print(got_coords)
-                    draw_polygon( got_coords )
+    count = 0
+    for polygon in data[state]: # полигон
+        count += 1
+        for point in polygon: # точка полигона
+            got_coords = transform(*point)
+            # print(got_coords)
+            draw_polygon( got_coords )
+    print(count)
 
 root.mainloop()
