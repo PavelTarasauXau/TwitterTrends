b7f6e81cb190885bc02d086e614f977ca455a178
Pavel Tarasau <zefirfricadelkovich@gmail.com>
2025-04-04 00:17:43 +0300

    almost final

--- main.py
+++ main.py
@@ -8,22 +8,21 @@ from ReadTweets import read_tweets
 from SentimentDict import load_sentiment_dict
 import csv
 import tkinter as tk
-
-
 from draw_map import creating_map
 
 def main():
-    creating_map()
-if __name__ == "__main__":
     file_name = get_tweet_file()
 
     tweets = read_tweets(file_name)
     sentiment_dict = load_sentiment_dict("sentiments.csv")
-    main()
-
 
     for tweet in tweets:
         print(tweet)
+
+    creating_map()
+
+if __name__ == "__main__":
+    main()
 
 
 
