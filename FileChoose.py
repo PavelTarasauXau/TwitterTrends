af84e108d3fa9a178b61f610cb4892e38a62b08d
Pavel Tarasau <zefirfricadelkovich@gmail.com>
2025-04-07 23:54:12 +0300

    get_tweet_file function fixed

--- FileChoose.py
+++ FileChoose.py
@@ -1,7 +1,6 @@
-# файл для выбора .txt файла
+import os
 
 def get_tweet_file():
-
     print("Выберите файл для анализа:")
     print("0 - cali_tweets2014.txt (по умолчанию)")
     print("1 - family_tweets2014.txt")
@@ -15,14 +14,28 @@ def get_tweet_file():
 
     choice = input("Введите номер (0-8) или название файла: ").strip()
 
+    base_path = "tweet_topics"
+
     match choice:
-        case "0": return "cali_tweets2014.txt"
-        case "1": return "family_tweets2014.txt"
-        case "2": return "football_tweets2014.txt"
-        case "3": return "high_school_tweets2014.txt"
-        case "4": return "movie_tweets2014.txt"
-        case "5": return "shopping_tweets2014.txt"
-        case "6": return "snow_tweets2014.txt"
-        case "7": return "texas_tweets2014.txt"
-        case "8": return "weekend_tweets2014.txt"
-        case _: return choice
+        case "0":
+            file_name = "cali_tweets2014.txt"
+        case "1":
+            file_name = "family_tweets2014.txt"
+        case "2":
+            file_name = "football_tweets2014.txt"
+        case "3":
+            file_name = "high_school_tweets2014.txt"
+        case "4":
+            file_name = "movie_tweets2014.txt"
+        case "5":
+            file_name = "shopping_tweets2014.txt"
+        case "6":
+            file_name = "snow_tweets2014.txt"
+        case "7":
+            file_name = "texas_tweets2014.txt"
+        case "8":
+            file_name = "weekend_tweets2014.txt"
+        case _:
+            file_name = choice
+
+    return os.path.join(base_path, file_name)
