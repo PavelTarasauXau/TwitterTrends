126baea167cd4084f93bf3e1432522ec1ca9da07
Pavel Tarasau <zefirfricadelkovich@gmail.com>
2025-04-09 16:56:56 +0300

    already better

--- Tweet.py
+++ Tweet.py
@@ -1,5 +1,5 @@
-import bisect
 import re
+from BinarySearch import binary_search
 
 class Tweet:
     def __init__(self, location, dt, text):
@@ -12,37 +12,33 @@ class Tweet:
         keys = list(sentiment_dict.keys())
         sentiment_score = 0.0
 
+        # Очищаем текст и разбиваем на слова
         text_without_punctuation = re.sub(r"[^\w\s'-]", "", self.text)
         words = text_without_punctuation.lower().split()
+        words_len = len(words)
 
         i = 0
-        while i < len(words):
-
-            remaining_words = len(words) - i
-
-            if remaining_words > 7:
-                phrase_run = 7
-            else:
-                phrase_run = remaining_words
+        while i < words_len:
+            remaining_words = words_len - i
+            phrase_run = min(7, remaining_words)  # Максимальная длина фразы - 7 слов
 
             matched = False
-
-            while phrase_run > 0:
+            while phrase_run > 0 and not matched:
                 phrase = ' '.join(words[i:i + phrase_run])
-                idx = bisect.bisect_left(keys, phrase)
+                # Используем нашу функцию бинарного поиска вместо bisect
+                idx = binary_search(keys, phrase)
 
-                if idx < len(keys) and keys[idx] == phrase:
+                if idx != -1:  # Фраза найдена
                     sentiment_score += sentiment_dict[phrase]
-                    i += phrase_run
+                    i += phrase_run  # Перескакиваем на конец найденной фразы
                     matched = True
-                    break
                 else:
-                    phrase_run -= 1
+                    phrase_run -= 1  # Пробуем более короткую фразу
 
             if not matched:
-                i += 1
+                i += 1  # Не нашли фразу, переходим к следующему слову
 
         self.sentiment = sentiment_score if sentiment_score != 0.0 else None
-
     def __repr__(self):
         return f"Tweet({self.location}, {self.datetime}, Sentiment={self.sentiment}, '{self.text[:30]}...')"
+
